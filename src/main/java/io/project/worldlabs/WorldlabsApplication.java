eff59cab7740c977032f2ab2ee669eb8445a3165
keshegs <instaspinhelp@gmail.com>
2022-12-06 13:47:48 +0300

    first commit

---
+++ src/main/java/io/project/worldlabs/WorldlabsApplication.java
@@ -0,0 +1,13 @@
+package io.project.worldlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldlabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldlabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/project/worldlabs/WorldlabsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.worldlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldlabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
